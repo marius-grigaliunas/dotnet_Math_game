2cecee643f526ce5aecffcfd3fad103a2e6781aa
Marius Grigaliūnas <cia.marius.grigaliunas@gmail.com>
2024-10-12 17:59:13 +0200

    Input class start, continuous running

---
+++ src/Input.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Game.src
+{
+    internal class Input
+    {
+        public static void CheckInput(string input, out bool end, out string returnMessage)
+        {
+            returnMessage = "";
+            end = false;
+
+            switch(input)
+            {
+                case "exit":
+                    end = true;
+                    returnMessage = "Program ended press any button to close.";
+                    break;
+                default:
+                    returnMessage = "command not recognized, you can type 'help' for a list of commands";
+                    break;
+            }
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Math_Game.src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowWidth = 100;
+
+            Console.WriteLine("Math Game");
+
+            Console.WriteLine("To exit the app enter   exit");
+
+            bool endApp = false;
+
+            while (!endApp)
+            {
+                Console.WriteLine("Your input:");
+                string input = Console.ReadLine() ?? "";
+
+                Input.CheckInput(input, out endApp, out string returnMessage);
+                Console.WriteLine(returnMessage);
+            }
+        }
+    }
+}
